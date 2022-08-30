c3d5399242cfa3a317d6625cc286b5c395b3b876
yassin samir <yassinsam3@gmail.com>
2022-08-30 13:10:56 +0200

    adding a ts version

---
+++ t.js
@@ -0,0 +1,71 @@
+var main = document === null || document === void 0 ? void 0 : document.querySelector(".input");
+var input = document === null || document === void 0 ? void 0 : document.querySelector("#name");
+var name1 = document === null || document === void 0 ? void 0 : document.querySelector(".name");
+var num = document === null || document === void 0 ? void 0 : document.getElementById("num");
+var num1 = document === null || document === void 0 ? void 0 : document.querySelector(".num");
+var mm = document === null || document === void 0 ? void 0 : document.querySelector("#mm");
+var mm1 = document === null || document === void 0 ? void 0 : document.querySelector(".f");
+var mm2 = document === null || document === void 0 ? void 0 : document.querySelector(".ff");
+var yy = document === null || document === void 0 ? void 0 : document.querySelector("#yy");
+var cvv = document === null || document === void 0 ? void 0 : document.querySelector("#cvv");
+var span = document === null || document === void 0 ? void 0 : document.querySelector(".back span");
+var inputs = document === null || document === void 0 ? void 0 : document.querySelectorAll("input");
+var form = document === null || document === void 0 ? void 0 : document.querySelector("form");
+var con = document === null || document === void 0 ? void 0 : document.querySelector(".con");
+(input && num && mm && yy && cvv).value = "";
+input.oninput = function () {
+    name1.textContent = input.value || "Jane Appleseed";
+};
+function rotate() {
+    con.style.transform = "rotateY(0deg)";
+}
+function logic(e, num) {
+    var retur = new RegExp("(d{".concat(num, "})"), "g");
+    return e.replace(/[A-Za-z]/g, "").replace(retur, "$1");
+}
+num.addEventListener("focus", rotate);
+num.oninput = function (e) {
+    e.target.value = e.target.value
+        .replace(/[^\dA-Z]/g, "")
+        .replace(/(\d{4})/g, "$1 ")
+        .trim();
+    if (Number(e.target.value.replace(/\s/g, ""))) {
+        num1.textContent = e.target.value;
+    }
+    else
+        num1.textContent = "0000 0000 0000 0000";
+};
+mm.addEventListener("focus", rotate);
+mm.oninput = function (e) {
+    e.target.value = logic(e.target.value, 2);
+    if (Number(e.target.value)) {
+        mm1.textContent = e.target.value;
+    }
+    else
+        mm1.textContent = "00";
+};
+yy.addEventListener("focus", rotate);
+yy.oninput = function (e) {
+    e.target.value = logic(e.target.value, 2);
+    if (Number(e.target.value)) {
+        mm2.textContent = e.target.value;
+    }
+    else
+        mm2.textContent = "00";
+};
+cvv.addEventListener("focus", function () { return (con.style.transform = "rotateY(180deg)"); });
+cvv.oninput = function (e) {
+    e.target.value = logic(e.target.value, 3);
+    if (Number(e.target.value)) {
+        span.textContent = e.target.value;
+    }
+    else
+        span.textContent = "000";
+};
+inputs.forEach(function (i) { return (i.required = true); });
+form.onsubmit = function (e) {
+    e.preventDefault();
+    main.style.display = "none";
+    var confirm = document.querySelector(".confirm");
+    confirm.style.display = "grid";
+};
---
+++ t.ts
@@ -0,0 +1,79 @@
+const main: HTMLDivElement = document?.querySelector(
+  ".input"
+) as HTMLDivElement;
+const input: HTMLInputElement = document?.querySelector(
+  "#name"
+) as HTMLInputElement;
+const name1: HTMLSpanElement = document?.querySelector(
+  ".name"
+) as HTMLSpanElement;
+const num: HTMLInputElement = document?.getElementById(
+  "num"
+) as HTMLInputElement;
+const num1: HTMLSpanElement = document?.querySelector(
+  ".num"
+) as HTMLSpanElement;
+const mm: HTMLInputElement = document?.querySelector("#mm") as HTMLInputElement;
+const mm1: HTMLSpanElement = document?.querySelector(".f") as HTMLSpanElement;
+const mm2: HTMLSpanElement = document?.querySelector(".ff") as HTMLSpanElement;
+const yy: HTMLInputElement = document?.querySelector("#yy") as HTMLInputElement;
+const cvv: HTMLInputElement = document?.querySelector(
+  "#cvv"
+) as HTMLInputElement;
+const span: HTMLSpanElement = document?.querySelector(
+  ".back span"
+) as HTMLSpanElement;
+const inputs: NodeList = document?.querySelectorAll("input") as NodeList;
+const form: HTMLFormElement = document?.querySelector(
+  "form"
+) as HTMLFormElement;
+const con: HTMLDivElement = document?.querySelector(".con") as HTMLDivElement;
+(input && num && mm && yy && cvv).value = "";
+input.oninput = (): void => {
+  name1.textContent = input.value || "Jane Appleseed";
+};
+function rotate(): void {
+  con.style.transform = "rotateY(0deg)";
+}
+function logic(e: string, num: number): string {
+  let retur = new RegExp(`(\d{${num}})`, "g");
+  return e.replace(/[A-Za-z]/g, "").replace(retur, "$1");
+}
+num.addEventListener("focus", rotate);
+num.oninput = (e: any): void => {
+  e.target.value = e.target.value
+    .replace(/[^\dA-Z]/g, "")
+    .replace(/(\d{4})/g, "$1 ")
+    .trim();
+  if (Number(e.target.value.replace(/\s/g, ""))) {
+    num1.textContent = e.target.value;
+  } else num1.textContent = "0000 0000 0000 0000";
+};
+mm.addEventListener("focus", rotate);
+mm.oninput = (e: any): void => {
+  e.target.value = logic(e.target.value, 2);
+  if (Number(e.target.value)) {
+    mm1.textContent = e.target.value;
+  } else mm1.textContent = "00";
+};
+yy.addEventListener("focus", rotate);
+yy.oninput = (e: any): void => {
+  e.target.value = logic(e.target.value, 2);
+  if (Number(e.target.value)) {
+    mm2.textContent = e.target.value;
+  } else mm2.textContent = "00";
+};
+cvv.addEventListener("focus", () => (con.style.transform = "rotateY(180deg)"));
+cvv.oninput = (e: any): void => {
+  e.target.value = logic(e.target.value, 3);
+  if (Number(e.target.value)) {
+    span.textContent = e.target.value;
+  } else span.textContent = "000";
+};
+inputs.forEach((i: any): boolean => (i.required = true));
+form.onsubmit = (e: any): void => {
+  e.preventDefault();
+  main.style.display = "none";
+  const confirm = document.querySelector(".confirm") as HTMLDivElement;
+  confirm.style.display = "grid";
+};
